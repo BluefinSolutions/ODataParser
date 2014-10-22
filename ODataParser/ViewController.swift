e8a3d2966280bb00f34638e6c994dc27c9711b22
Brenton O'Callaghan <callaghan001@gmail.com>
2014-10-22 13:45:21 -0700

    Initial Commit

---
+++ ODataParser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ODataParser
+//
+//  Created by Brenton O'Callaghan on 22/10/2014.
+//  Copyright (c) 2014 The SCN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
